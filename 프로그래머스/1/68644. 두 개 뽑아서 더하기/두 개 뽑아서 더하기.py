54fe530fb1cd5b13a2a77c825c25a74c78da2bb1
Sojeong Lee <dlgur200123@naver.com>
2024-07-10 18:47:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,7 +1,3 @@
 def solution(numbers):
     answer = []
-    a=len(numbers)
-    for i in range(a):
-        for j in range(i+1,a):
-            answer.append(numbers[i]+numbers[j])
-    return sorted(list(set(answer)))
+    return answer
