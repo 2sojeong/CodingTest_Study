dd7e6c5f1e3205814f24b3bba7333f989145afb2
Sojeong Lee <dlgur200123@naver.com>
2024-09-20 18:43:53 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+   public static void main(String[] args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+       long tep[]=new long[n+1];
+        tep[0]=0;
+        tep[1]=1;
+        for(int i=2;i<=n;i++){
+            tep[i]=tep[i-1]+tep[i-2];
+        }
+        System.out.print(tep[n]);
+    }
+
+}
