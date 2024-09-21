3ca465a703727c443985b698e0e73a2769e15086
Sojeong Lee <dlgur200123@naver.com>
2024-09-21 17:19:45 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n + 2];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2])%10007;
+        }
+        System.out.print(arr[n] % 10007);
+    }
+}
