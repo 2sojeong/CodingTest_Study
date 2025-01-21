d5aeb636df6e2cfad7aedddaecc2b588504b95b6
Sojeong Lee <dlgur200123@naver.com>
2025-01-21 18:05:17 +0900

    [Silver III] Title: N과 M (5), Time: 364 ms, Memory: 41956 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] arr = new int[9];
+
+    static int[] num;
+
+    static StringBuilder sb = new StringBuilder();
+
+    static boolean[] visit = new boolean[9];
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(num);
+
+        bt(0);
+        System.out.print(sb);
+        
+
+    }
+
+    static void bt(int k){
+        if(k == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++){
+            if(!visit[i]){
+                arr[k] = num[i];
+                visit[i] = true;
+                bt(k+1);
+                visit[i] = false;
+            }
+        }
+    }
+
+}
