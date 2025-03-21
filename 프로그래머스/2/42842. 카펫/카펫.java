d7e526bc11e17211b3fbee566bfdf2f12afb22bc
Sojeong Lee <dlgur200123@naver.com>
2025-03-21 15:22:08 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,18 @@
-import java.util.*;
-import java.io.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int total = brown + yellow;
-        //i는 세로 , j는 가로
+        // i 가로, j 세로
         for(int i = 3; i < total; i++){
-            int j = total / i ;
+            int j = total / i;
             
-            if (total % i == 0 && j >= 3){
-                if(j < i){
-                    continue;
-                }
-                if((j-2) * (i-2) == yellow){
-                    answer[0] = j;
-                    answer[1] = i;
+            if(j > i){
+                continue;
+            }
+            if(total % i == 0 && j >= 3){
+                if((i-2)*(j-2) == yellow){
+                    answer[0] = i;
+                    answer[1] = j;
                     return answer;
                 }
             }
