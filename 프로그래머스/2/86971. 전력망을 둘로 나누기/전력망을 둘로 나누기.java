6eea9894ee56d69806d80d27e1da6f2673f701b5
Sojeong Lee <dlgur200123@naver.com>
2025-03-21 18:27:04 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 15.35 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,55 +1,50 @@
 import java.util.*;
-
+import java.io.*;
 
 class Solution {
-    private static int[][] map;
-    
+    static int[][] map;
+    static boolean[] visit;
     public int solution(int n, int[][] wires) {
         map = new int[n+1][n+1];
+        visit = new boolean[n+1];
         int answer = n;
-
         for(int i = 0; i < wires.length; i++){
-            int v1 = wires[i][0];
-            int v2 = wires[i][1];
+            int a = wires[i][0];
+            int b = wires[i][1];
             
-            map[v1][v2] = 1;
-            map[v2][v1] = 1;
+            map[a][b] = 1;
+            map[b][a] = 1;
         }
         
         for(int i = 0; i < wires.length; i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
-            
             map[v1][v2] = 0;
             map[v2][v1] = 0;
             
-            answer = Math.min(answer,bfs(n,v1));
-            
+            int result = bfs(v1,n);
+            answer = Math.min(answer,result);
             map[v1][v2] = 1;
             map[v2][v1] = 1;
-            
-        }
-        
+        }        
         
         return answer;
     }
     
-    private static int bfs(int n, int x){
-        boolean[] visit = new boolean[n+1];
-        int cnt = 1;
-        
+    static int bfs(int x, int n){
+        visit = new boolean[n+1];
         Queue<Integer> queue = new LinkedList<>();
         visit[x] = true;
         queue.add(x);
-        
+        int cnt = 1;
         while(!queue.isEmpty()){
             int temp = queue.poll();
             
-            for(int i = 1; i <= n; i++){
+            for(int i = 1; i <= n ; i++){
                 if(map[temp][i] == 1 && !visit[i]){
                     visit[i] = true;
-                    queue.add(i);
                     cnt ++;
+                    queue.add(i);
                 }
             }
         }
