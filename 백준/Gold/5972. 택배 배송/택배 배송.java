86ea09b721a7b2a0e060d64047db82cf1a28f606
Sojeong Lee <dlgur200123@naver.com>
2025-05-30 15:31:27 +0900

    [Gold V] Title: 택배 배송, Time: 368 ms, Memory: 40032 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -2,64 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     private static int N,M;
-    private static int[] distance;
+    private static boolean[] visit;
+    private static int[] dis;
+
     private static class Node implements Comparable<Node>{
-        int t;
-        int weight;
+        int to;
+        int cost;
 
-        public Node(int t, int weight){
-            this.t = t;
-            this.weight = weight;
+        public Node(int to, int cost){
+            this.to = to;
+            this.cost = cost;
         }
-
-        public int compareTo(Node node){
-            return this.weight - node.weight;
+        public int compareTo(Node t){
+            return this.cost - t.cost;
         }
     }
 
-    private static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-
+    private static ArrayList<Node>[] graph;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        distance = new int[N+1];
-        for(int i = 0; i < N+1; i++){
-            graph.add(new ArrayList<>());
-            distance[i] = Integer.MAX_VALUE;
+        graph = new ArrayList[N+1];
+        dis = new int[N+1];
+        visit = new boolean[N+1];
+
+        for(int i = 0; i <= N; i++){
+            graph[i] = new ArrayList<>();
+            dis[i] = Integer.MAX_VALUE;
         }
-        distance[1] = 0;
+
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(new Node(b,c));
-            graph.get(b).add(new Node(a,c));
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[from].add(new Node(to,cost));
+            graph[to].add(new Node(from,cost));
         }
 
-        find();
-        System.out.print(distance[N]);
+        find(1);
+        System.out.print(dis[N]);
     }
 
-    private static void find(){
-            PriorityQueue<Node> queue = new PriorityQueue<>();
-            queue.add(new Node(1,0));
+    private static void find(int start){
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start,0));
+        dis[start] = 0;
 
-            while(!queue.isEmpty()){
-                Node now = queue.poll();
+        while(!queue.isEmpty()){
+            Node temp = queue.poll();
 
-                for(Node next : graph.get(now.t)){
-                    if(distance[next.t] > distance[now.t] + next.weight){
-                        distance[next.t] = distance[now.t] + next.weight;
-                        queue.add(new Node(next.t, distance[next.t]));
-                    }
+            if(visit[temp.to]) continue;
+            visit[temp.to] = true;
+
+            for(Node next : graph[temp.to]){
+                if(dis[next.to] > dis[temp.to] + next.cost){
+                    dis[next.to] = dis[temp.to] + next.cost;
+
+                    queue.add(new Node(next.to, dis[next.to]));
                 }
             }
+        }
+
     }
 }
