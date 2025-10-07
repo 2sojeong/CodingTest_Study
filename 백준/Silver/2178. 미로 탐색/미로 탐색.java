00e6c0143b88ba78530ed9dbe58786a1dee5e5ac
Sojeong Lee <dlgur200123@naver.com>
2025-10-07 23:36:10 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,82 +1,70 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int M;
 
-    public static boolean[][] visit=new boolean[101][101];
-
-    public static int[][] map=new int[101][101];
-
-    public static int res;
-
-    public static int[] dx = {0,0,1,-1};
-    public static int[] dy = {1,-1,0,0};
-
-    public static void main(String[] args) throws IOException{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        for(int i = 1; i <= N; i++){
-            String temp = br.readLine();
-            for(int j = 1; j <= M ; j++){
-                map[i][j] = temp.charAt(j-1) - '0';
-            }
-        }
-
-  
-
-        bfs();
-        System.out.print(res);
-
-
-    }
-
-    static void bfs(){
-        Queue<Node> queue = new LinkedList<>();
-        visit[1][1] = true;
-        queue.add(new Node(1,1,1));
-
-        while (!queue.isEmpty()){
-
-            Node cur = queue.poll();
-
-            if(cur.x == N && cur.y == M){
-                res = cur.t;
-            }
-            for(int i = 0; i < 4; i++){
-                int x = cur.x + dx[i];
-                int y = cur.y + dy[i];
-
-                if(check(x,y)){
-                    visit[x][y] = true;
-                    queue.add(new Node(x,y,cur.t +1));
-                }
-
-            }
-        }
-    }
-
-    static boolean check(int x, int y){
-        return x >= 1 && x <= N && y >=1 && y <= M && !visit[x][y] && map[x][y] == 1;
-    }
-
-    static class Node {
+    private static int N, M;
+    private static int[][] map;
+    private static int[] dx = {0,0,1,-1};
+    private static int[] dy = {1,-1,0,0};
+    private static class Node {
         int x;
         int y;
         int t;
 
-        public Node(int x, int y, int t) {
+        public Node (int x, int y, int t) {
             this.x = x;
             this.y = y;
             this.t = t;
         }
+    }
+    private static Queue<Node> queue = new LinkedList<>();
+    private static boolean[][] visit;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visit = new boolean[N][M];
+        for(int i = 0; i < N; i++) {
+            String temp = br.readLine();
+            for(int j = 0; j < M; j++) {
+                map[i][j] = temp.charAt(j) - '0';
+            }
+        }
+        bfs();
+    }
+
+    private static void bfs() {
+        queue.add(new Node(0,0,1));
+        visit[0][0] = true;
+
+        while(!queue.isEmpty()) {
+            Node temp = queue.poll();
+
+            if(temp.x == N - 1 && temp.y == M - 1) {
+                System.out.print(temp.t);
+                return;
+            }
+            for(int i = 0; i < 4; i++) {
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+
+                if(check(nx,ny)) {
+                    visit[nx][ny] = true;
+                    queue.add(new Node(nx,ny,temp.t + 1));
+                }
+            }
+        }
+    }
+
+    private static boolean check(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M && !visit[x][y] && map[x][y] == 1;
     }
 }
